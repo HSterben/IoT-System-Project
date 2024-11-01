fd4837c6974e1b3acde02a576b90c939444883e2
Liam Webster <112972528+LiamWebster25@users.noreply.github.com>
2024-11-01 11:40:34 -0400

    Updated Email. It works now
    Added some documentation making the 2 methods more clear.

--- Phase 2/Email.py
+++ Phase 2/Email.py
@@ -2,13 +2,19 @@ import smtplib
 import ssl
 import imaplib
 import email
+import time
 from email.message import EmailMessage
 
 class Email:
-    EMAIL = "liamgroupiot@gmail.com"
-    PASSWORD = "unip eiah qvyn bjbp"
-    SERVER = 'smtp.gmail.com'
+    EMAIL = "liamgroupiot@gmail.com" # Gmail used to send the email
+    PASSWORD = "unip eiah qvyn bjbp" # APP password for liamgroupiot@gmail.com
+    SERVER = 'smtp.gmail.com' # Google's gmail SMTP server address
 
+# Send an email
+#
+# Takes parameter: temp as the current temperature emmited by the DHT11
+# Sends an email to parameter: email_receiver
+# Returns: Nothing (void)
     def send_email(self, temp, email_receiver):
         # Variable with email sender
         email_sender = self.EMAIL
@@ -25,20 +31,24 @@ class Email:
 
         # Create a secure SSL context
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(self.SERVER, 465, context=context) as smtp:  # Use port 465 for SMTP_SSL
-            # Login into the sender email
+        with smtplib.SMTP_SSL(self.SERVER, 465, context=context) as smtp:  # Port 465 for SMTP_SSL
+            # Login into liamgroupiot@gmail.com
             smtp.login(email_sender, email_password)
-            # Send the email
+            # Send the email to email_receiver
             smtp.sendmail(email_sender, email_receiver, em.as_string())
 
+# Check the sent email's response
+#
+# Takes parameter: sender_email which is the email used to reply (email we sent to)
+# Returns: True if the user replies 'YES' to the sent email or 'None' otherwise
     def receive_email(self, sender_email):
         # Connect to the server and go to its inbox
         mail = imaplib.IMAP4_SSL(self.SERVER)
         mail.login(self.EMAIL, self.PASSWORD)
-        # Choose the inbox
+        # Selects inbox
         mail.select('inbox')
 
-        # Search for unseen emails from the specified sender with the specified subject
+        # Search the inbox for a specific email
         status, data = mail.search(None, 'UNSEEN', f'HEADER SUBJECT "Temperature Is Getting High"', f'HEADER FROM "{sender_email}"')
 
         mail_ids = []
@@ -67,20 +77,5 @@ class Email:
                     else:
                         mail_content = message.get_payload(decode=True).decode()
 
+                    #Return  True if the user replies 'YES' to the sent email
                     return "yes" in mail_content.lower()
-
-def main():
-    email_client = Email()
-    email_client.send_email(25)  # Send an email with the temperature of 25 degrees
-
-    for count in range(4):
-        if not email_client.receive_email("websterliam25@gmail.com"):  # Check for a response
-            time.sleep(10)
-            print(count)
-        else:
-            print("Fan has been turned on!")
-            break
-
-
-if __name__ == "__main__":
-    main()  # Run the main function
