b6b9f4d3239337b785f0b41a1692cf4bbeb38c18
Sterben <142906656+HSterben@users.noreply.github.com>
2024-11-25 14:22:24 -0500

    update

--- Phase 4/Database_add_data.py
+++ Phase 4/Database_add_data.py
@@ -1,3 +1,3 @@
 import setup_db as db
-db.insert_user_threshold("dashboard.db", "David", 25.0, 50.0, 200, "f357b54")
-db.insert_user_threshold("dashboard.db", "Logan", 23.0, 80.0, 50, "2ca53e4a")
+db.insert_user_threshold("dashboard.db", "User1", 25.0, 50.0, 200, "f357b54")
+db.insert_user_threshold("dashboard.db", "User2", 23.0, 80.0, 50, "2ca53e4a")
