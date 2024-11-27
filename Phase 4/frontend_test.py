dc180665a16792ff384f5c1de216ca831700d444
Fruan Tabamo Jr <fruhamsan@gmail.com>
2024-11-27 11:29:22 -0500

    Updated frontend

--- Phase 4/frontend_test.py
+++ Phase 4/frontend_test.py
@@ -8,17 +8,15 @@ import random
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO], suppress_callback_exceptions=True)
 app.title = "Raspberry Pi IoT Dashboard"
 
-# App Layout Front-End
+
+# Dash application
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
+
 app.layout = dbc.Container(fluid=True, children=[
-    # Add the Interval component to trigger periodic updates
-    dcc.Interval(id='update-interval', interval=2000, n_intervals=0),  # 2 seconds interval
-
     html.H1('Raspberry Pi IoT Dashboard Phase #2', className='text-center my-4'),
-
-    # Row containing the Side Panel (User Profile) and Main Content Area
+    
     dbc.Row([
-        # Side Panel (User Profile) - Fixed on the left
-        dbc.Col([
+         dbc.Col([
             dbc.Card([
                 dbc.CardHeader("User Profile", style={'font-size': '24px'}, className="text-center"),
                 dbc.CardBody([
@@ -39,106 +37,71 @@ app.layout = dbc.Container(fluid=True, children=[
        
    
         dbc.Col([
-            dbc.Tabs([
-                dbc.Tab(label="Temperature & Humidity", tab_id="temperature-humidity-tab"),
-                dbc.Tab(label="Light Intensity", tab_id="light-intensity-tab"),
-                dbc.Tab(label="Bluetooth", tab_id="bluetooth-tab"),
-                dbc.Tab(label="Settings", tab_id="settings-tab")
-            ], id="user-profile-tabs", active_tab="temperature-humidity-tab"),
-
-            # Content for the active tab
-            html.Div(id="tab-content")
-        ], width=9, style={'margin-left': '25%'}),  # Adjust the margin to account for the side panel width
+            dbc.Card([
+                dbc.CardHeader("Temperature and Humidity", style={'font-size': '24px'}, className="text-center"),
+                dbc.CardBody(
+                    dbc.Row([
+                        dbc.Col(
+                            html.Div([
+                                html.Label('Temperature (°C)', className='label'),  
+                                daq.Gauge(
+                                    id='temp-gauge', 
+                                    min=0, 
+                                    max=50, 
+                                    value=0, 
+                                    style={'margin-bottom': '1px'},
+                                    color={'gradient': True, 'ranges': {'red': [0, 20], 'yellow': [20, 35], 'green': [35, 50]}}
+                                ),
+                                html.Div(id='temp-display', className='text-center', style={'font-size': '20px', 'margin-top': '1px', 'color': 'white'})  # Numerical temperature reading
+                            ], className='card-body-center'),
+                            width=6
+                        ),
+                        dbc.Col(
+                            html.Div([
+                                html.Label('Humidity (%)', className='label'),  
+                                daq.Gauge(
+                                    id='humidity-gauge', 
+                                    min=0, 
+                                    max=100, 
+                                    value=0, 
+                                    style={'margin-bottom': '1px'},
+                                    color={'gradient': True, 'ranges': {'red': [0, 40], 'yellow': [40, 70], 'green': [70, 100]}}
+                                ),
+                                html.Div(id='humidity-display', className='text-center', style={'font-size': '20px', 'margin-top': '1px', 'color': 'white'})  # Numerical humidity reading
+                            ], className='card-body-center'),
+                            width=6
+                        )
+                    ])
+                )
+            ])
+        ], width=12)
     ], className="mb-4"),
-
-    # Footer
+    # Fan control card
+    dbc.Row(
+        dbc.Col(BL_notification, width=6, lg=3),
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader("Fan Control", className="text-center", style={'font-size': '24px'}),
+                dbc.CardBody([
+                    html.Img(id='fan-image', src='/assets/fan_off_spinning.png', style={'display': 'block', 'margin': '20px auto', 'width': '200px'}),
+                    html.Div(id='fan-status', className='text-center', children="The fan is currently turned OFF."),
+                ])
+            ])
+        )
+    , className="mb-4"),
+    dcc.Interval(
+        id='update-interval',
+        interval=5*1000,  # Updates every 5 seconds
+        n_intervals=0
+    ),
     dbc.Row([
         dbc.Col([
             html.Footer("Powered by Raspberry Pi", className="text-center text-muted", style={'margin-top': '30px'})
         ], width=12)
     ])
 ])
-
-# Tab Content based on active tab
-@app.callback(
-    Output('tab-content', 'children'),
-    [Input('user-profile-tabs', 'active_tab')]
-)
-def render_content(tab):
-    if tab == 'temperature-humidity-tab':
-        return html.Div([
-            # Fan Control and Gauges on Dashboard Tab
-            html.Div([
-                dbc.Card([
-                    dbc.CardHeader("Temperature and Humidity", style={'font-size': '24px'}, className="text-center"),
-                    dbc.CardBody(
-                        dbc.Row([
-                            dbc.Col(
-                                html.Div([
-                                    html.Label('Temperature (°C)', className='label'),
-                                    daq.Gauge(
-                                        id='temp-gauge',
-                                        min=0,
-                                        max=50,
-                                        value=0,
-                                        style={'margin-bottom': '1px'},
-                                        color={'gradient': True, 'ranges': {'red': [0, 20], 'yellow': [20, 35], 'green': [35, 50]}}
-                                    ),
-                                    html.Div(id='temp-display', className='text-center',
-                                             style={'font-size': '20px', 'margin-top': '1px', 'color': 'white'})
-                                ], className='card-body-center'),
-                                width=6
-                            ),
-                            dbc.Col(
-                                html.Div([
-                                    html.Label('Humidity (%)', className='label'),
-                                    daq.Gauge(
-                                        id='humidity-gauge',
-                                        min=0,
-                                        max=100,
-                                        value=0,
-                                        style={'margin-bottom': '1px'},
-                                        color={'gradient': True, 'ranges': {'red': [0, 40], 'yellow': [40, 70], 'green': [70, 100]}}
-                                    ),
-                                    html.Div(id='humidity-display', className='text-center',
-                                             style={'font-size': '20px', 'margin-top': '1px', 'color': 'white'})
-                                ], className='card-body-center'),
-                                width=6
-                            )
-                        ])
-                    )
-                ])
-            ], className="mb-4"),
-
-            html.Div([
-                # Fan Control Section
-                dbc.Card([
-                    dbc.CardHeader("Fan Control", className="text-center", style={'font-size': '24px'}),
-                    dbc.CardBody([
-                        html.Img(id='fan-image', src='/assets/fan_off_spinning.png',
-                                 style={'display': 'block', 'margin': '20px auto', 'width': '200px'}),
-                        html.Div(id='fan-status', className='text-center', children="The fan is currently turned OFF."),
-                    ])
-                ])
-            ], className="mb-4")
-        ])
-    elif tab == 'light-intensity-tab':
-        return html.Div([
-            html.H5("Settings", className="text-center"),
-            html.P("to be added.")
-        ])
-    elif tab == 'bluetooth-tab':
-        return html.Div([
-            html.H5("Settings", className="text-center"),
-            html.P("to be added.")
-        ])
         
-    elif tab == 'settings-tab':
-        return html.Div([
-            html.H5("Settings", className="text-center"),
-            html.P("to be added.")
-        ])
-
+    
 # Fake data for gauge and fan image 
 @app.callback(
     [Output('temp-gauge', 'value'),
